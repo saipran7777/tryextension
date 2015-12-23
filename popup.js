b522c7c1a2141a1582213c9c18efd405ec30e216
sai007 <sai_praneeth7777@yahoo.co.in>
2015-12-23 16:13:46 +0530

    xml 2

--- popup.js
+++ popup.js
@@ -4,13 +4,26 @@ function sendRequest() {
     console.log(username);
     var params="rollno="+username+"&pwd="+password;
     var url="https://www.iitm.ac.in/viewgrades/studentauth/login.php";
+    var url1="https://www.iitm.ac.in/viewgrades/studentauth/studopts2.php"
 
-
-    var xhr = new XMLHttpRequest();    
+    var xhr = new XMLHttpRequest(); 
+    var http = new XMLHttpRequest();   
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4) {
         // innerText does not let the attacker inject HTML elements.
         document.getElementById("resp").innerText = xhr.responseText;
+
+        http.onreadystatechange=function(){
+          if (http.readyState == 4) {
+            document.getElementById("resp1").innerText = http.responseText;
+          }
+          else{
+            document.getElementById("resp1").innerText = "Failed to get Response inside";
+          }
+        };
+        http.open("GET",url,true);
+        http.send();
+
       }
       else{
         document.getElementById("resp").innerText = "Failed to get Response";
@@ -18,8 +31,6 @@ function sendRequest() {
     };
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.setRequestHeader("Content-length", params.length);
-    xhr.setRequestHeader("Connection", "close");
     xhr.send(params);
 }
 
