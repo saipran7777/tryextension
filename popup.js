d7e18d6781ae6d02e4134242f96a4fd7a0d9f8ef
sai007 <sai_praneeth7777@yahoo.co.in>
2015-12-26 11:56:48 +0530

    Change of settings when roll no is changed

--- popup.js
+++ popup.js
@@ -11,6 +11,18 @@ var th = ['','thousand','million', 'billion','trillion'];
 
 var dg = ['th','First','Second','Third','Fourth', 'Fifth','Sixth','Seventh','Eighth','Ninth']; var tn = ['Tenth','Eleventh','Twelveth','Thirteenth', 'Fourteenth','Fifteenth','Sixteenth', 'Seventeenth','Eighteenth','Nineteenth']; var tw = ['Twenty','Thirty','Forty','Fifty', 'Sixty','Seventy','Eighty','ninety']; function toWords(s){s = s.toString(); s = s.replace(/[\, ]/g,''); if (s != parseFloat(s)) return 'not a number'; var x = s.indexOf('.'); if (x == -1) x = s.length; if (x > 15) return 'too big'; var n = s.split(''); var str = ''; var sk = 0; for (var i=0; i < x; i++) {if ((x-i)%3==2) {if (n[i] == '1') {str += tn[Number(n[i+1])] + ' '; i++; sk=1;} else if (n[i]!=0) {str += tw[n[i]-2] + ' ';sk=1;}} else if (n[i]!=0) {str += dg[n[i]] +' '; if ((x-i)%3==0) str += 'hundred ';sk=1;} if ((x-i)%3==1) {if (sk) str += th[(x-i-1)/3] + ' ';sk=0;}} if (x != s.length) {var y = s.length; str += 'point '; for (var i=x+1; i<y; i++) str += dg[n[i]] +' ';} return str.replace(/\s+/g,' ');}
 
+var savedrollno = "";
+var savedpwd = "";
+
+chrome.storage.sync.get('rollno', function (result) {
+savedrollno = result.rollno;
+console.log("Saved rollno is"+result.rollno);
+});
+
+
+chrome.storage.sync.get('pwd', function (result) {
+savedpwd = result.pwd;
+});
 
 
 
@@ -18,10 +30,12 @@ var dg = ['th','First','Second','Third','Fourth', 'Fifth','Sixth','Seventh','Eig
 var num = toWords(12);
 console.log(num);
 function sendRequest() {
+
     document.getElementById("resp1").innerText ="";
     var username= document.getElementById("rollno").value;
     var password= document.getElementById("pwd").value;
     console.log(username);
+
     var useryear = username.substring(2,4);
     console.log(useryear);
       var d = new Date();
@@ -53,6 +67,21 @@ function sendRequest() {
         http.onreadystatechange=function(){
 
           if (http.readyState == 4) {
+
+            chrome.storage.sync.set({'rollno': username,'pwd': password}, function() {
+
+            document.getElementById("test").innerHTML="Settings saved";
+            var namsespace="sync";
+          if (savedrollno != username ) {
+            alert("Roll no used has been changed and set");
+            }
+          else{
+           alert("Username and password are set");
+         }
+
+          });
+
+
             var searchto = "<center>";
             console.log(searchto);
             var str =http.responseText;
@@ -82,9 +111,11 @@ function sendRequest() {
               });
 
           }
+
           else{
             document.getElementById("resp1").innerText = "Failed to get Response inside";
           }
+
         };
         http.open("GET",url1,true);
         http.send();
@@ -108,14 +139,27 @@ document.getElementById("submit").addEventListener("click",sendRequest);
 });
 
 chrome.storage.onChanged.addListener(function(changes, namespace) {
+
+var username= document.getElementById("rollno").value;
+
   for (key in changes) {
+
     var storageChange = changes[key];
+
+    if (savedrollno === username) {
     console.log('Storage key "%s" in namespace "%s" changed. ' +
                 'Old value was "%s", new value is "%s".',
                 key,
                 namespace,
                 storageChange.oldValue,
                 storageChange.newValue);
+    }
+
+    else{
+      console.log('Roll no is changed from "%s" to "%s".',savedrollno,username);
+    }
+
   }
+
 });
 
